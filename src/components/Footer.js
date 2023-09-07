36dc6e3aa0803f03d5715db904dddf119fc91031
AgusRodrigues <agusrodrigues199@gmail.com>
2023-09-07 20:12:18 -0300

    tailwind update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div className='flex justify-between items-center h-14 bg-slate-600 mt-11 px-5'>
-      <p>Copyright Maimo 2023</p>
-      <p>Prog 3</p>
-    </div>
-  )
-}
-
-export default Footer
+    <section class="bg-gray-900">
+      <div class="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden">
+        <nav class="flex flex-wrap justify-center -mx-5 -my-2">
+          <div class="px-5 py-2">
+            <a
+              href="#"
+              class="text-base leading-6 text-gray-500 hover:text-white"
+            >
+              About
+            </a>
+          </div>
+          <div class="px-5 py-2">
+            <a
+              href="#"
+              class="text-base leading-6 text-gray-500 hover:text-white"
+            >
+              Contact
+            </a>
+          </div>
+          <div class="px-5 py-2">
+            <a
+              href="#"
+              class="text-base leading-6 text-gray-500 hover:text-white"
+            >
+              Terms
+            </a>
+          </div>
+        </nav>
+        <p class="mt-8 text-base leading-6 text-center text-gray-400">
+          © 2023 TheBestShows, Inc. All rights reserved.
+        </p>
+      </div>
+    </section>
+  );
+};
+export default Footer;
